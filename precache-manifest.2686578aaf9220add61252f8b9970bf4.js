1b6af04072ad167886a88a0ce027394f168c8531
Thada Wangthammang <mildronize@gmail.com>
2019-04-02 14:48:05 +0700

    Updates

---
+++ precache-manifest.2686578aaf9220add61252f8b9970bf4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "6b287286c26cfbf1b57c",
+    "url": "/text-tools/static/css/main.5fac1af2.chunk.css"
+  },
+  {
+    "revision": "6b287286c26cfbf1b57c",
+    "url": "/text-tools/static/js/main.899cbd66.chunk.js"
+  },
+  {
+    "revision": "dc0b49dbdee3774de0ef",
+    "url": "/text-tools/static/js/runtime~main.f06ccdb4.js"
+  },
+  {
+    "revision": "d217b82e69d9e84250f4",
+    "url": "/text-tools/static/css/2.185b9dce.chunk.css"
+  },
+  {
+    "revision": "d217b82e69d9e84250f4",
+    "url": "/text-tools/static/js/2.5ca56bd8.chunk.js"
+  },
+  {
+    "revision": "0667c19323f5297b22ad605ed8ce384c",
+    "url": "/text-tools/index.html"
+  }
+];
